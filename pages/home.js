23c9638a4dbf468a42c5a4c33fb4531a1d6fb91c
Vinay Vishwakarma <vinay@onecorp.co.in>
2023-02-11 21:05:12 +0530

    amp

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,26 @@
-import React from 'react'
 
-const home = () => {
+import { useAmp } from 'next/amp'
+export const config = { amp: 'hybrid' }
+
+function About(props) {
+  const isAmp = useAmp()
+
   return (
-    <div>this is home page made by vinay</div>
+    <div>
+      <h3>My AMP About Page!</h3>
+      {isAmp ? (
+        <amp-img
+          width="300"
+          height="300"
+          src="https://images.unsplash.com/photo-1676085272023-91fce74ee32b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+          alt="a cool image"
+          layout="responsive"
+        />
+      ) : (
+        <img width="300" height="300" src="https://plus.unsplash.com/premium_photo-1661962548081-071712b709ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=2000&q=60" alt="a cool image" />
+      )}
+    </div>
   )
 }
 
-export default home
+export default About
