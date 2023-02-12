36d40f2adfb957c1e1348c474d6f3c64fa2b3414
Vinay Vishwakarma <vinay@onecorp.co.in>
2023-02-12 17:01:40 +0530

    story

--- pages/web/[...id].js
+++ pages/web/[...id].js
@@ -12,62 +12,62 @@ function About(props) {
 
   return (
     <> <Head>
-    <script async src="https://cdn.ampproject.org/v0.js"></script>
-    <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script> 
-   
-    <link rel="icon" href="/Times_Ascent_Icon.png" />
-   
-    <title>klkl</title>
-   
-    <link rel="icon" href="/public/Times_Ascent_Icon.png" />
-   
-   
+      <script async src="https://cdn.ampproject.org/v0.js"></script>
+      <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
 
-    <script async custom-element="amp-video"  src="https://cdn.ampproject.org/v0/amp-video-0.1.js"></script>
-    <script async custom-element="amp-story"  src="https://cdn.ampproject.org/v0/amp-story-1.0.js"></script>
-        <link
+      <link rel="icon" href="/Times_Ascent_Icon.png" />
+
+      <title>klkl</title>
+
+      <link rel="icon" href="/public/Times_Ascent_Icon.png" />
+
+
+
+      <script async custom-element="amp-video" src="https://cdn.ampproject.org/v0/amp-video-0.1.js"></script>
+      <script async custom-element="amp-story" src="https://cdn.ampproject.org/v0/amp-story-1.0.js"></script>
+      <link
         rel="dns-prefetch"
         href="https://www.google-analytics.com/analytics.js"
       />
 
-     <link rel="canonical" href = "/"/>
-  
+      <link rel="canonical" href="/dfg" />
 
 
-  
-  </Head> <div>
 
-      <h3>My AMP About Page!</h3>
-      {isAmp ? (
-       WebStoriesDetails.map((i)=> <amp-story 
-       standalone={false}
-       publisher = ""
-       title="Times Ascent"
-       publisher-logo-src="https://timesascent.com/newimages/main/times_ascent_logo.svg"
-       poster-portrait-src="https://timesascent.com/newimages/main/times_ascent_logo.svg"> <amp-story-page id="dfd" auto-advance-after="5s">     <amp-story-grid-layer 
-       template="responsive"
-       ><amp-img
-       className="ampimg"
-       width="300"
-       height="300"
-       src={i.DImage}
-       alt="a cool image"
-       layout="responsive"
-     /></amp-story-grid-layer> </amp-story-page></amp-story>) 
-      ) : (
-        <img width="300" height="300" src="https://plus.unsplash.com/premium_photo-1661962548081-071712b709ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=2000&q=60" alt="a cool image" />
-      )}
-    </div></>
-   
+
+    </Head> <div>
+
+        <h3>My AMP About Page!</h3>
+        {isAmp ? (
+          WebStoriesDetails.map((i) => <amp-story
+            standalone={false}
+            publisher=""
+            title="Times Ascent"
+            publisher-logo-src="https://timesascent.com/newimages/main/times_ascent_logo.svg"
+            poster-portrait-src="https://timesascent.com/newimages/main/times_ascent_logo.svg"> <amp-story-page id="dfd" auto-advance-after="5s">     <amp-story-grid-layer
+              
+            ><amp-img
+                className="ampimg"
+                width="300"
+                height="300"
+                src={i.DImage}
+                alt="a cool image"
+                layout="responsive"
+              /></amp-story-grid-layer> </amp-story-page></amp-story>)
+        ) : (
+          <img width="300" height="300" src="https://plus.unsplash.com/premium_photo-1661962548081-071712b709ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=2000&q=60" alt="a cool image" />
+        )}
+      </div></>
+
   )
 }
 export async function getServerSideProps(context) {
-    const WebStoryDetailListdata = await webStoriesDetailApi({
-      WebStoriesSummaryID: context.query.id[1],
-    });
-  
-    return {
-      props: { WebStoryDetailListdata },
-    };
-  }
+  const WebStoryDetailListdata = await webStoriesDetailApi({
+    WebStoriesSummaryID: context.query.id[1],
+  });
+
+  return {
+    props: { WebStoryDetailListdata },
+  };
+}
 export default About
